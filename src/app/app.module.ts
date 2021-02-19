9ac1a0698f375130c56f1fe2ad9216d9040a83ee
rus <rautov74@gmail.com>
2021-02-19 17:46:19 +0600

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { PromotionService } from './services/promotion.service';
+import {LeaderService} from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,7 @@ import { PromotionService } from './services/promotion.service';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
