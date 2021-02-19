b56fb3404dc768c5acd7e1262102f83ef29ef3a2
rus <rautov74@gmail.com>
2021-02-19 17:29:09 +0600

    pre Assignment 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,9 @@
-import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetail',
@@ -9,12 +12,19 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-    @Input()
     dish!: Dish;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    constructor(private dishservice: DishService,
+      private route: ActivatedRoute,
+      private location: Location) { }
+  
+    ngOnInit() {
+      const id = this.route.snapshot.params['id'];
+      this.dish = this.dishservice.getDish(id);
+    }
+  
+    goBack(): void {
+      this.location.back();
+    }
 
 }
